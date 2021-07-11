70c5e9090d5cb594159df1c6dc7b30a94adddff1
matej.zacek <matej.zacek@kiwi.com>
2021-07-11 21:20:46 +0200

    Update syntax check

--- src/syntax_check.hpp
+++ src/syntax_check.hpp
@@ -6,12 +6,12 @@ namespace foc {
 
 bool syntax_check(const VarDecl& decl, std::shared_ptr<IDContext> context);
 bool syntax_check(const Assign& ass, std::shared_ptr<IDContext> context);
-bool syntax_check(const IfCond& ifflow, std::shared_ptr<IDContext> context, bool in_cycle);
-bool syntax_check(const Cond& cond, std::shared_ptr<IDContext> context, bool in_cycle);
-bool syntax_check(const Loop& loop, std::shared_ptr<IDContext> context, bool in_cycle);
-bool syntax_check(const Flow::Control& ctrl, std::shared_ptr<IDContext> context, bool in_cycle);
-bool syntax_check(const Flow& flow, std::shared_ptr<IDContext> context, bool in_cycle);
-bool syntax_check(const FunBody& body, std::shared_ptr<IDContext> context, bool in_cycle);
+bool syntax_check(const IfCond& ifflow, std::shared_ptr<IDContext> context, bool in_cycle, const Type& ret_type);
+bool syntax_check(const Cond& cond, std::shared_ptr<IDContext> context, bool in_cycle, const Type& ret_type);
+bool syntax_check(const Loop& loop, std::shared_ptr<IDContext> context, bool in_cycle, const Type& ret_type);
+bool syntax_check(const Flow::Control& ctrl, std::shared_ptr<IDContext> context, bool in_cycle, const Type& ret_type);
+bool syntax_check(const Flow& flow, std::shared_ptr<IDContext> context, bool in_cycle, const Type& ret_type);
+bool syntax_check(const FunBody& body, std::shared_ptr<IDContext> context, bool in_cycle, const Type& ret_type);
 bool syntax_check(const FunDecl& fun_decl, std::shared_ptr<IDContext> par_context);
 bool syntax_check(const Program& prog);
 
@@ -21,5 +21,7 @@ std::optional<Type> get_expr_type(const FunCall& expr, std::shared_ptr<IDContext
 std::optional<Type> get_expr_type(const TypeExpr& expr, std::shared_ptr<IDContext> context);
 std::optional<Type> get_expr_type(const ID& expr, std::shared_ptr<IDContext> context);
 std::optional<Type> get_expr_type(const Expr& expr, std::shared_ptr<IDContext> context);
+
+bool is_lvalue(const Expr& expr);
 
 }
