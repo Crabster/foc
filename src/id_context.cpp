b3d0744a0a81d948e91266b33a6ce4c5d72b3337
matej.zacek <matej.zacek@kiwi.com>
2021-07-17 17:01:25 +0200

    Add option to more debug info to context struct

--- src/id_context.cpp
+++ src/id_context.cpp
@@ -3,6 +3,21 @@
 
 namespace foc {
 
+IDContext::IDContext(bool do_debug) {
+    debug = do_debug;
+}
+
+IDContext::IDContext() {
+    debug = false;
+}
+
+IDContext::~IDContext() {
+    if (debug) {
+        std::cout << "Destructing context with:\n";
+        print_self();
+    }
+}
+
 std::optional<Type> IDContext::find_type(const ID& id) const {
     auto it = type_decls.find(id);
     if (it != type_decls.end()) {
@@ -29,6 +44,9 @@ void IDContext::add_context(const ID& id, const Type& type) {
     if (is_declared(id)) {
         std::cerr << "Warning: shadowing name `" << id.name << "`." << std::endl;
     }
+    if (debug) {
+        std::cout << "Context: Adding " << id.name << " with type " << type.to_string() << std::endl;
+    }
     type_decls[id] = type;
 }
 
@@ -37,6 +55,9 @@ bool IDContext::add_strict_context(const ID& id, const Type& type) {
     if (was_declared) {
         std::cerr << "ShadowError: name `" << id.name << "` should have type `" <<
         find_type(id)->to_string() << "` as well as `" << type.to_string() << "`." << std::endl;
+    }
+    if (debug) {
+        std::cout << "Context: Strictly adding " << id.name << " with type " << type.to_string() << std::endl;
     }
     type_decls[id] = type;
     return !was_declared;
@@ -68,6 +89,22 @@ bool IDContext::add_contexts(const std::vector<ID>& ids, const Type& type) {
     } else {
         std::cerr << "TieError: trying to use tie for non-array and non-tuple type." << std::endl;
         return false;
+    }
+}
+
+void IDContext::print_self() const {
+    for (const auto& [id, type] : type_decls) {
+        std::cout << id.name << " :: " << type.to_string() << std::endl;
+    }
+}
+
+void IDContext::debug_print() const {
+    std::cout << "-------------------------\n";
+    print_self();
+    if (parent_context) {
+        parent_context->debug_print();
+    } else {
+        std::cout << "-------------------------" << std::endl;
     }
 }
 
--- src/id_context.hpp
+++ src/id_context.hpp
@@ -6,14 +6,22 @@
 namespace foc {
 
 struct IDContext {
+    IDContext(bool debug);
+    IDContext();
+    ~IDContext();
+
     std::shared_ptr<IDContext> parent_context;
     std::unordered_map<ID, Type> type_decls;
+    bool debug;
 
     std::optional<Type> find_type(const ID& id) const;
     bool is_declared(const ID& id) const;
     void add_context(const ID& id, const Type& type);
     bool add_strict_context(const ID& id, const Type& type);
     bool add_contexts(const std::vector<ID>& id, const Type& type);
+
+    void debug_print() const;
+    void print_self() const;
 };
 
 }
