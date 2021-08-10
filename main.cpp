44e400681d56d2177f5345dbd6595147bc582d56
matej.zacek <matej.zacek@kiwi.com>
2021-08-10 23:20:34 +0200

    Add help and more arguments

--- main.cpp
+++ main.cpp
@@ -1,22 +1,84 @@
 #include <cstdlib>
 #include <iostream>
+#include <sstream>
 #include "FocLexer.h"
 #include "src/code_visitor.hpp"
 #include "src/code_generator.hpp"
 #include "src/syntax_check.hpp"
 
+void print_help() {
+    std::cout << "This is a compiler for a `Foc` language.\n";
+    std::cout << "Usage: ./build/demo [args]\n";
+    std::cout << "Possible arguments:\n";
+    std::cout << "\t -h \t\t -> Prints help\n";
+    std::cout << "\t -i `path` \t -> Compiles file on `path`\n";
+    std::cout << "\t -d \t\t -> Enables debug mode for the compiler\n";
+    std::cout << "\t -e `num` \t -> Compilation stops after `num` errors (default 10)" << std::endl;
+}
+
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
+    if (argc == 1) {
+        std::cout << "Invalid use, no arguments" << std::endl;
+        print_help();
         return 1;
     }
 
-    std::string file_name = argv[1];
+    std::string file_name;
+    bool set_filename = false;
+    unsigned limit = 10;
+    bool debug_mode = false;
+
+    for (unsigned i = 1; i < argc; ++i) {
+        std::string curr = argv[i];
+        if (curr == "-h") {
+            print_help();
+            return 1;
+        } else if (curr == "-d") {
+            debug_mode = true;
+        } else if (curr == "-e") {
+            if (i + 1 >= argc) {
+                std::cout << "Invalid use, argument `-e` without number" << std::endl;
+                print_help();
+                return 1;
+            }
+            std::stringstream strVal;
+            strVal << argv[i+1];
+            strVal >> limit;
+            if (strVal.fail()) {
+                std::cout << "Invalid use, argument after `-e` isn't unsigned number" << std::endl;
+                print_help();
+                return 1;
+            }
+            ++i;
+        } else if (curr == "-i") {
+            if (i + 1 >= argc) {
+                std::cout << "Invalid use, argument `-i` without path" << std::endl;
+                print_help();
+                return 1;
+            }
+            file_name = argv[i+1];
+            set_filename = true;
+            ++i;
+        } else {
+            std::cout << "Invalid use, unknown argument `" << curr << "`" << std::endl;
+            print_help();
+            return 1;
+        }
+    }
+
+    if (!set_filename) {
+        std::cout << "Invalid use, no filepath given" << std::endl;
+        print_help();
+        return 1;
+    }
+
     std::ifstream stream;
     stream.open(file_name);
-
-    // Maybe make option to get these from command line
-    unsigned limit = 1;
-    bool debug_mode = false;
+    if (stream.fail()) {
+        std::cout << "Couldn't open file `" << file_name << "`" << std::endl;
+        print_help();
+        return 1;
+    }
 
     antlr4::ANTLRInputStream input(stream);
 
